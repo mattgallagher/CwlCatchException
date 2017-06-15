909653d63fe5b0481415b8e0e855bcf5c6812a64
Matt Gallagher <matt@zqueue.com>
2017-06-15 14:53:02 +1000

    Revert "Updated to new packge format."
    This reverts commit 18b36ed0e552639bfbbbb022a5de79c636de05eb.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,9 @@
-// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
 	name: "CwlCatchException",
-	products: [.library(name: "CwlCatchException", targets: ["CwlCatchException"])],
 	targets: [
-		.target(name: "CwlCatchExceptionSupport"),
-		.target(name: "CwlCatchException", dependencies: ["CwlCatchExceptionSupport"]),
+		Target(name: "CwlCatchException", dependencies: ["CwlCatchExceptionSupport"]),
+		Target(name: "CwlCatchExceptionSupport")
 	]
 )
