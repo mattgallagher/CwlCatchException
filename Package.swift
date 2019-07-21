f809deb30dc5c9d9b78c872e553261a61177721a
Matt Gallagher <matt@zqueue.com>
2019-07-21 13:43:12 +1000

    Move to static library, and distribution via Swift Package Manager, only.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
 	name: "CwlCatchException",
 	products: [
-		.library(name: "CwlCatchException", type: .dynamic, targets: ["CwlCatchException"]),
+		.library(name: "CwlCatchException", targets: ["CwlCatchException"]),
 	],
 	targets: [
 		.target(name: "CwlCatchException", dependencies: [.target(name: "CwlCatchExceptionSupport")]),
